7e45942989db5161663a6ee9b36a5311e7f48655
Tin <tinfants123@gmail.com>
2023-10-26 12:48:31 +0200

    connecting to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 import express from "express"
-import { PORT } from "./config.js"
+import { PORT, mongoDBURL } from "./config.js"
+import mongoose, { Mongoose } from "mongoose";
 
 const app = express()
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+app.get('/', (req, res) => {
+    console.log(req)
+    return res.send('Hello World')
+});
 
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+    console.log("MongoDB Connected");
+
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`)
+    });
+}) 
+.catch((err) => {console.log(err)});
