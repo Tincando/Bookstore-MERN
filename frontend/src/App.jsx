498b51cba088e10e156776a8257b38db4d9e0d37
Tin <tinfants123@gmail.com>
2023-10-28 14:04:33 +0200

    Adding pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import CreateBook from './Pages/CreateBook'
+import ShowBook from './Pages/ShowBook'
+import EditBook from './Pages/EditBook'
+import DeleteBooks from './Pages/DeleteBooks'
+
 
 export const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline bg-red-400">
-    Hello world!
-  </h1>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='books/create' element={<CreateBook />} />
+      <Route path='books/details/:id' element={<ShowBook />} />
+      <Route path='books/edit/:id' element={<EditBook />} />
+      <Route path='books/delete/:id' element={<DeleteBooks />} />
+    </Routes>
   )
 }
 
